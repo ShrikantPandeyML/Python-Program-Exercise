168cd7145950fef5f2dc8ab3fce07353d2345712
Shrikant Pandey <shrikantpandeymnnit2015@gmail.com>
2018-02-28 19:42:02 +0530

    Print the calendar of a given month and year

---
+++ Basic Program/calendar.py
@@ -0,0 +1,6 @@
+import calendar
+year = int(input("Enter the year"))
+
+# enter the month in numeric value such 03,10,11 etc
+month1 = int(input ("Enter the month"))
+print("year and month",calendar.month(year,month1))
